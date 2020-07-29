2a5a82126803a050acc189e8dabd48f60baf1f45
Juan Guillermo Del Valle Urrego <juangui93@gmail.com>
2020-07-28 20:50:03 -0500

    Se valida que todos los campos esten llenos

--- calificacionEstudiantes/Form1.cs
+++ calificacionEstudiantes/Form1.cs
@@ -19,20 +19,35 @@ namespace calificacionEstudiantes
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            double nota1, nota2, nota3, nota4, suma = 0, promedio = 0;
 
-            nota1 = Double.Parse(txtNota1.Text);
-            nota2 = Double.Parse(txtNota2.Text);
-            nota3 = Double.Parse(txtNota3.Text);
-            nota4 = Double.Parse(txtNota4.Text);
+            if (txtNota1.Text == "" 
+                || txtNota2.Text == "" || 
+                txtNota3.Text == "" || txtNota4.Text == "")
+            {
+                MessageBox.Show("Debes diligenciar todos los campos",
+                    "Error del sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            else
+            {
+                double nota1, nota2, nota3, nota4, suma = 0, promedio = 0;
 
-            suma = (nota1 + nota2 + nota3 + nota4);
+                nota1 = Double.Parse(txtNota1.Text);
+                nota2 = Double.Parse(txtNota2.Text);
+                nota3 = Double.Parse(txtNota3.Text);
+                nota4 = Double.Parse(txtNota4.Text);
 
-            promedio = suma / 4;
+                suma = (nota1 + nota2 + nota3 + nota4);
 
-            lblPromedio.Text = Convert.ToString(promedio);
-            label6.Visible = true; 
-            lblPromedio.Visible = true; 
+                promedio = suma / 4;
+
+                lblPromedio.Text = Convert.ToString(promedio);
+                label6.Visible = true;
+                lblPromedio.Visible = true;
+            }
+           
+            
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
